f867c7fb819713f07b4dc72a2a00b59ed96bbfc8
oliveiratullio <105726160+oliveiratullio@users.noreply.github.com>
2025-08-23 18:56:27 -0300

    fix: jest config

---
+++ packages/backend/jest.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import("jest").Config} */
+module.exports = {
+    preset: "ts-jest/presets/default-esm",
+    testEnvironment: "node",
+    extensionsToTreatAsEsm: [".ts"],
+    transform: {
+        "^.+\\.(t|j)s$": ["ts-jest", { useESM: true, tsconfig: "tsconfig.json" }]
+    },
+    moduleFileExtensions: ["ts", "js", "json"],
+    rootDir: ".",
+    testRegex: ".*\\.spec\\.ts$",
+    collectCoverageFrom: ["src/**/*.(t|j)s"],
+    coverageDirectory: "coverage",
+
+    // 👇 permite importar com ".js" nos arquivos TS
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1"
+    }
+};
