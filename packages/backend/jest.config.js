fc01c130a10d88d5916cb919630e95f0ddc6ffaa
oliveiratullio <105726160+oliveiratullio@users.noreply.github.com>
2025-08-23 19:07:46 -0300

    fix: jest config

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -1,19 +1,17 @@
-/** @type {import("jest").Config} */
+/** @type {import('jest').Config} */
 export default {
-    preset: "ts-jest/presets/default-esm",
-    testEnvironment: "node",
-    extensionsToTreatAsEsm: [".ts"],
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
     transform: {
-        "^.+\\.(t|j)s$": ["ts-jest", { useESM: true, tsconfig: "tsconfig.json" }]
+        '^.+\\.(t|j)s$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.json' }],
     },
-    moduleFileExtensions: ["ts", "js", "json"],
-    rootDir: ".",
-    testRegex: ".*\\.spec\\.ts$",
-    collectCoverageFrom: ["src/**/*.(t|j)s"],
-    coverageDirectory: "coverage",
-
-    // 👇 permite importar com ".js" nos arquivos TS
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    rootDir: '.',
+    testRegex: '.*\\.spec\\.ts$',
+    collectCoverageFrom: ['src/**/*.(t|j)s'],
+    coverageDirectory: 'coverage',
     moduleNameMapper: {
-        "^(\\.{1,2}/.*)\\.js$": "$1"
-    }
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
 };
