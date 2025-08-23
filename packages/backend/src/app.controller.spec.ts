e93f7f9b145569d8fc9888b65826e956543f12b9
oliveiratullio <105726160+oliveiratullio@users.noreply.github.com>
2025-08-23 19:17:45 -0300

    fix: test

--- packages/backend/src/app.controller.spec.ts
+++ packages/backend/src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -14,7 +14,7 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  it('should return "Hello World!"', () => {
-    expect(appController.health).toBe({ status: 'ok', version: '0.1.0' });
+  it('should return { status: "ok", version: "0.1.0" }', () => {
+    expect(appController.health()).toEqual({ status: 'ok', version: '0.1.0' });
   });
 });
