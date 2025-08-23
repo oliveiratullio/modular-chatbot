183a434387226c8cc314975ead85aadf8e2bea6a
oliveiratullio <105726160+oliveiratullio@users.noreply.github.com>
2025-08-23 17:27:30 -0300

    chore: enviroment variables

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,14 +1,15 @@
-import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import type { FastifyInstance } from 'fastify';
+import fastifyCors from '@fastify/cors';
 import fastifyHelmet from '@fastify/helmet';
-import fastifyCors, { type FastifyCorsOptions } from '@fastify/cors';
-import { ValidationPipe, type INestApplication } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -16,29 +17,21 @@ async function bootstrap() {
     new FastifyAdapter({ logger: true }),
   );
 
-  // instância Fastify tipada (para registrar plugins)
-  const fastify = app
-    .getHttpAdapter()
-    .getInstance() as unknown as FastifyInstance;
-  await fastify.register(fastifyHelmet);
+  await app.register(fastifyHelmet);
 
-  const corsOpts: FastifyCorsOptions = {
-    origin: process.env.CORS_ORIGIN
-      ? process.env.CORS_ORIGIN.split(',').map((s) => s.trim())
-      : true,
+  const origins =
+    process.env.CORS_ORIGIN?.split(',').map((o) => o.trim()) ?? true;
+
+  await app.register(fastifyCors, {
+    origin: origins,
     methods: ['GET', 'POST', 'OPTIONS'],
-  };
-  await fastify.register(fastifyCors, corsOpts);
+    credentials: true,
+  });
 
-  // ✅ cast seguro p/ interface do Nest (expõe useGlobalPipes)
-  const nestApp = app as unknown as INestApplication;
-  nestApp.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, transform: true }),
-  );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const port = Number(process.env.PORT ?? 8080);
   await app.listen(port, '0.0.0.0');
-
-  console.log(`Backend up on http://localhost:${port}`);
+  console.log(`Server running on port: ${port}`);
 }
 bootstrap();
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function App() {
-  const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8080";
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const [status, setStatus] = useState<string>("");
 
